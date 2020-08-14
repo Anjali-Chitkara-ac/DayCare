a09de17a04e8c7ab54e0acad64d5377a02a4c9eb
manasa <manasabhat794@gmail.com>
2020-08-14 01:00:42 -0400

    person package commit- 12AMEST commit

--- src/main/java/edu/neu/csye6200/daycare/person/Person.java
+++ src/main/java/edu/neu/csye6200/daycare/person/Person.java
@@ -1,9 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.neu.csye6200.daycare.person;
-
+/**
+ *
+ * @author anjali
+ */
 public abstract class Person {
-    private String name;
-    private int age;
-    private String sex;
+    public String name;
+    public int age;
+    public String sex;
 
     public String getName() {
         return name;
